de3ac22fa625797a59c267fb9aa459f7c1fb9be7
Marcello Urbani <marcello.urbani@gmail.com>
2025-06-03 09:05:00 +0100

    initial working server with dev

---
+++ README.md
@@ -0,0 +1,4 @@
+# MCP server for SAP dumps
+
+This will enable MCP clients to analyse SAP dumps.
+Inspired by [mcp-windbg](https://github.com/svnscha/mcp-windbg), explained in [this blog](https://svnscha.de/posts/ai-meets-windbg/)
---
+++ src/index.ts
@@ -0,0 +1,105 @@
+import express from "express"
+import { Server } from "@modelcontextprotocol/sdk/server/index.js"
+import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js"
+import {
+  isInitializeRequest,
+  ListResourcesRequestSchema,
+  ReadResourceRequestSchema
+} from "@modelcontextprotocol/sdk/types.js"
+import { v4 } from "uuid"
+import { z } from "zod"
+
+const createServer = () => {
+  const server = new Server(
+    {
+      name: "sapdump-mcp-server",
+      version: "1.0.0"
+    },
+    {
+      capabilities: {
+        resources: {},
+        tools: {}
+      }
+    }
+  )
+
+  server.setRequestHandler(ListResourcesRequestSchema, async () => {
+    return {
+      resources: [
+        {
+          uri: "file:///Users/Documents/my-mcp-server/logs.txt",
+          name: "Application Logs",
+          mimeType: "text/plain"
+        }
+      ]
+    }
+  })
+
+  // Read resource contents
+  server.setRequestHandler(ReadResourceRequestSchema, async request => {
+    const uri = request.params.uri
+    if (uri === "file:///Users/Documents/my-mcp-server/logs.txt") {
+      const logContents = "dummy log entry"
+      return { contents: [{ uri, mimeType: "text/plain", text: logContents }] }
+    }
+    throw new Error("Resource not found")
+  })
+  return server
+}
+
+const app = express()
+app.use(express.json())
+
+const transports: Map<string, StreamableHTTPServerTransport> = new Map()
+
+const getTransport = async (sessionId: string, body: unknown) => {
+  const current = transports.get(sessionId)
+  if (current) return current
+  if (!isInitializeRequest(body))
+    throw new Error("Invalid session ID or request body")
+
+  const newTransport = new StreamableHTTPServerTransport({
+    sessionIdGenerator: () => v4(),
+    onsessioninitialized: sessionId => transports.set(sessionId, newTransport)
+  })
+  newTransport.onclose = () =>
+    newTransport.sessionId && transports.delete(newTransport.sessionId)
+
+  const server = createServer()
+  await server.connect(newTransport)
+
+  return newTransport
+}
+
+app.post("/mcp", async (req, res) => {
+  try {
+    const sessionId = req.headers["mcp-session-id"] as string | undefined
+    const transport = await getTransport(sessionId || "", req.body)
+    await transport.handleRequest(req, res, req.body)
+  } catch (error) {
+    console.error("Error handling MCP request:", error)
+    res.status(400).json({
+      jsonrpc: "2.0",
+      error: {
+        code: -32000,
+        message: "Bad Request: No valid session ID provided"
+      },
+      id: null
+    })
+  }
+})
+const handleSessionRequest = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  const sessionId = req.headers["mcp-session-id"]
+  const transport = transports.get(`${sessionId}`)
+  if (!transport) {
+    res.status(400).send("Invalid or missing session ID")
+    return
+  }
+  await transport.handleRequest(req, res)
+}
+app.get("/mcp", handleSessionRequest)
+app.delete("/mcp", handleSessionRequest)
+app.listen(3000, () => console.log("MCP server is running on port 3000"))
